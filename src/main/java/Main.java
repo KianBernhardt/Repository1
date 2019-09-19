6812dc8efdf4bbf17c01a112e9a7e19b272d7c85
Kian Bernhardt <87418@farnborough.ac.uk>
2019-09-19 10:29:05 +0100

    19/09/2019 - hopefully you can get up to date from this...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,18 @@ public class Main {
         openDatabase("Database.db");
 
         Users.listUsers();
-        deleteUser(2);
+
+        System.out.println("Adding user...");
+        Users.insertUser(10, "Steve", "Beans");
+        Users.listUsers();
+
+        System.out.println("Updating user...");
+        Users.updateUser(10, "SuperSteve", "Beans123");
+        Users.listUsers();
+
+        System.out.println("Deleting user...");
+        Users.deleteUser(10);
+        Users.listUsers();
 
         closeDatabase();
     }
@@ -36,59 +47,6 @@ public class Main {
         }
     }
 
-    public static void insertUser (int User_ID, String Username){
-        try{
-            PreparedStatement ps = db.prepareStatement("INSERT INTO Users (User_ID, Username) VALUES (?, ?)");
 
-            ps.setInt(1, User_ID);
-            ps.setString(2, Username);
-            ps.executeUpdate();
-            System.out.println("Record has been added to the 'Users' table");
-
-        } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
-            System.out.println("Error: Something has gone wrong.");
-        }
-    }
-
-    public static void listUsers() {
-        try {
-            PreparedStatement ps = db.prepareStatement("SELECT User_ID, Username, Password FROM Users");
-
-            ResultSet results = ps.executeQuery();
-            while (results.next()) {
-                int User_ID = results.getInt(1);
-                String Username = results.getString(2);
-                String Password = results.getString(3);
-                System.out.println(User_ID + " " + Username + " " + Password);
-            }
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
-        }
-    }
-
-    public static void deleteUser(int User_ID) throws SQLException {
-        try{
-            PreparedStatement ps = db.prepareStatement("DELETE FROM Users WHERE User_ID = ?");
-            ps.setInt(1, User_ID);
-
-        }catch (Exception exception){
-            System.out.println(exception.getMessage());
-        }
-    }
-
-    public static void updateUser (String Username, String Password){
-        try{
-            PreparedStatement ps = db.prepareStatement("UPDATE Users SET Username = ?, Password = ?");
-            ps.setString(1, Username);
-            ps.setString(2, Password);
-            ps.executeUpdate();
-            System.out.println("The 'Users' table has been updated");
-
-        } catch (SQLException exception) {
-            System.out.println(exception.getMessage());
-            System.out.println("Error: Something has gone wrong.");
-        }
-    }
 }
 
